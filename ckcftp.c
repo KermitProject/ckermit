3c871d977d4c8e84c9d830dae3686a63845222b9
Jeffrey Altman <jaltman@auristor.com>
2025-11-17 06:16:27 -0500

    ckcdeb.h: relocate sig_t typedef from ckcftp.c
    sig_t should be available wherever signal() is, but there is no harm
    in redefining a type if the definition has not changed.  This change
    relocates the sig_t typedef to ckcdeb.h from ckcftp.c so it can be
    used in all C-Kermit source files.
    
    Change-Id: If5ea96fc38877f608a6066b1ec14f6beb082e7fc

--- ckcftp.c
+++ ckcftp.c
@@ -27,7 +27,7 @@ char *ckftpv = "FTP Client, 10.0.281, 18 Sep 2023";
 
   . Implement recursive NLST downloads by trying to CD to each filename.
     If it works, it's a directory; if not, it's a file -- GET it.  But
-    that won't work with servers like wu-ftpd that don't send directory 
+    that won't work with servers like wu-ftpd that don't send directory
     names.  Recursion with MLSD is done.
 
   . Make syslog entries for session?  Files?
@@ -963,58 +963,6 @@ char * ftp_apw = NULL;			/* Anonymous password */
 
 /* Definitions and typedefs needed for prototypes */
 
-/*
-  #define sig_t my_sig_t
-
-  I don't understand the statement above, which has been in this code going
-  back to at least C-Kermit 8.0, because my_sig_t is not defined anywhere.
-  And yet sig_t is used below with no complaint, no matter whether the the
-  above #define is commented out or not.  However, if I #define sig_t SIGTYP
-  (which is what it should be according to ckcdeb.h), all hell breaks loose.
-  Same if I replace all references to sig_t by SIGTYPE.  On the other hand, if
-  I remove the sig_t definition, there is no complaint, so where is the sig_t
-  definition coming from?  I find this in Ubuntu signal.h:
-
-    <comment> 4.4 BSD uses the name 'sig_t' for this. </comment>
-    typedef __sighandler_t sig_t;
-
-  In NetBSD I find this in sys/signal.h:
-
-    typedef    void (*sig_t)(int);
-
-  Can we really count on sig_t being defined in some nook or cranny
-  on every single Unix, VMS, and Windows system?
-
-  Jeff Altman says to use sighandler_t, which is ok on Ubuntu but
-  not on (say) NetBSD.  So I can't do this:
-
-#ifndef sig_t
-#define sig_t sighandler_t
-#endif
-
-  I think the only alternative is to leave my_sig_t undefined and then
-  see who squawks.  Since the previous 'my_sig_t' definition apparently
-  had no effect, I'm hoping there will be no change in behavior after
-  commenting it out.    -fdc Fri Jun 23 06:53:23 2023
-
-  P.S. All this is aside from the fact that signal() has long since been
-  "deprecated" in favor of sigaction(), defined in POSIX.1-1988; see:
-
-    https://man7.org/linux/man-pages/man2/signal.2.html
-    https://pubs.opengroup.org/onlinepubs/9699919799/functions/sigaction.html
-
-  but C-Kermit can't be switched over because it has to build and run on
-  pre-POSIX operating systems that don't have sigaction(0) (despite the fact
-  that C-Kermit's ckupty.c function ptyint_vhangup() calls it...  how did
-  *that* happen?).
-*/
-
-#ifdef CK_ANSIC
-typedef SIGTYP (*sig_t)(int);
-#else
-typedef SIGTYP (*sig_t)();
-#endif /* CK_ANSIC */
-
 /* Made this global static -fdc 21 June 2023 */
 /* It's used in many ckcftp.c routines but wasn't declared in all of them */
 static sig_t oldintr;
@@ -1266,7 +1214,7 @@ FILE * fp_nml = NULL;                   /* Namelist file pointer */
 
 static int csocket = -1;                /* Control socket */
 static int connected = 0;               /* Connected to FTP server */
-/* static unsigned short ftp_port = 0; */ /* FTP port */ 
+/* static unsigned short ftp_port = 0; */ /* FTP port */
 /* static int ftp_port = 0; */		/* SMS 2007/02/15 */
 static int ftp_port = 0;		/* fdc 2007/08/30 */
 #ifdef FTPHOST
@@ -2916,7 +2864,7 @@ dosetftp() {
       }
 
       case FTS_BUG: {
-          if ((x = cmkey(ftpbugtab,nftpbug,"","",xxstring)) < 0) 
+          if ((x = cmkey(ftpbugtab,nftpbug,"","",xxstring)) < 0)
 	    return(x);
           switch (x) {
 #ifdef CK_SSL
@@ -3830,7 +3778,7 @@ syncdir(local,sim) char * local; int sim;
                         cdsimlvl++;
                     } else {
                         if (!doftpxmkd(p,0)) { /* Can't CD - try to create */
-			    debug(F110,"ftp syncdir mkdir failed",p,0); 
+			    debug(F110,"ftp syncdir mkdir failed",p,0);
 /*
   Suppose we are executing SEND /RECURSIVE.  Locally we have a directory
   FOO but the remote has a regular file with the same name.  We can't CD
@@ -3840,7 +3788,7 @@ syncdir(local,sim) char * local; int sim;
                             quiet = saveq;
                             return(0);
                         }
-			debug(F110,"ftp syncdir mkdir OK",p,0); 
+			debug(F110,"ftp syncdir mkdir OK",p,0);
                         if (fdispla == XYFD_B) {
                             printf(" CREATED DIRECTORY %s\n",p);
                         } else if (fdispla) {
@@ -3849,12 +3797,12 @@ syncdir(local,sim) char * local; int sim;
                             ftscreen(SCR_ST,ST_MSG,(CK_OFF_T)0,msgbuf);
                         }
                         if (!doftpcwd(p,0)) { /* Try again to CD */
-			    debug(F110,"ftp syncdir CD failed",p,0); 
+			    debug(F110,"ftp syncdir CD failed",p,0);
                             quiet = saveq;
                             return(0);
                         }
                         if (fdispla == XYFD_B) printf(" CWD %s\n",p);
-			debug(F110,"ftp syncdir CD OK",p,0); 
+			debug(F110,"ftp syncdir CD OK",p,0);
                     }
                 }
                 cdlevel++;
@@ -4251,7 +4199,7 @@ chkmodtime(local,remote,fc) char * local, * remote; int fc;
             */
 	    int xx;
 #ifdef COMMENT
-#ifdef DEBUG	    
+#ifdef DEBUG
 	    if (deblog) {
 		dbtime("LOCAL",tmlocal);
 		dbtime("REMOT",&tmremote);
@@ -5049,7 +4997,7 @@ zzsend(fd,c) int fd; CHAR c;
     if (iscanceled())                   /* Check for cancellation */
       return(-9);
 
-#ifdef FTP_TIMEOUT    
+#ifdef FTP_TIMEOUT
     ftp_timed_out = 0;
     if (check_data_connection(fd,1) < 0) {
 	ftp_timed_out = 1;
@@ -7772,7 +7720,7 @@ doftpget(cx,who) int cx, who;
 			tmpsrc = NULL;
 			src = mgetlist[mgetx];
 			usrpath = srclen+1;
-		    }			      
+		    }
 		}
 	    }
 /*
@@ -10424,7 +10372,7 @@ ftpopen(remote, service, use_tls) char * remote, * service; int use_tls;
 #ifdef FTP_SECURITY
         if (ftp_aut) {
             if (ftp_auth()) {
-                if (ftp_cry 
+                if (ftp_cry
 #ifdef OS2
                      && ck_crypt_is_installed()
 #endif /* OS2 */
@@ -10514,7 +10462,7 @@ ssl_auth() {
         ssl_ftp_ctx = NULL;
     }
 
-    /* The SSL_OP_DONT_INSERT_EMPTY_FRAGMENTS 
+    /* The SSL_OP_DONT_INSERT_EMPTY_FRAGMENTS
      * was added to OpenSSL 0.9.6e and 0.9.7.  It does not exist in previous
      * versions
      */
@@ -10701,7 +10649,7 @@ ssl_auth() {
                 printf("?Unable to load crl-dir: %s\r\n",path);
         }
 #endif /* NT */
-        
+
         ckmakmsg(path,CKMAXPATH,exedir,"ca_crls.pem",NULL,NULL);
         if (X509_STORE_load_locations(crl_store,path,NULL) == 0) {
             debug(F110,"ftp ssl auth unable to load file",path,0);
@@ -11202,7 +11150,7 @@ parsefacts(s) char * s;
 
     while ((p = ckstrchr(s,'='))) {
 	*p = NUL;			/* s points to fact */
-	i = xlookup(facttab,s,nfacttab,&x); 
+	i = xlookup(facttab,s,nfacttab,&x);
 	debug(F111,"ftp parsefact fact",s,i);
 	*p = '=';
 	s = p+1;			/* Now s points to arg */
@@ -12122,7 +12070,7 @@ doftpsend2(threadinfo) VOID * threadinfo;
     register int c, d = 0;
     int n, x, notafile, unique = 0;
     char *buf, *bufp;
-    
+
 #ifdef NTSIG
     if (threadinfo) {                   /* Thread local storage... */
         TlsSetValue(TlsIndex,threadinfo);
@@ -12240,7 +12188,7 @@ doftpsend2(threadinfo) VOID * threadinfo;
 	  which means STOU is not accepted, period.
 	*/
 	if ((x == 5) && stouarg && (ftpcode != 502)) {
-	    x = ftpcmd(ftpsnd.cmd,NULL,ftpsnd.incs,ftpsnd.outcs,ftp_vbm); 
+	    x = ftpcmd(ftpsnd.cmd,NULL,ftpsnd.incs,ftpsnd.outcs,ftp_vbm);
 	    if (x == REPLY_PRELIM)	/* If accepted */
 	      stouarg = 0;		/* flag no STOU arg for this server */
 	}
@@ -12291,7 +12239,7 @@ doftpsend2(threadinfo) VOID * threadinfo;
         errno = d = 0;
 #ifdef VMS
 	/*
-	  This is because VMS zxin() is C-Library fread() 
+	  This is because VMS zxin() is C-Library fread()
           but the file was opened with zopeni(), which is RMS.
 	*/
 	while (((c = zminchar()) > -1) && !cancelfile) {
@@ -12494,7 +12442,7 @@ sendrequest( cmd, local, remote, xlate, incs, outcs, restart )
     if (sndfilter)                      /* If sending thru a filter */
       pipesend = 1;                     /* set this for open and i/o */
 #endif /* PIPESEND */
-    
+
 #ifdef VMS
     debug(F101,"XXX before openi binary","",binary);
     debug(F101,"XXX before openi ftp_typ","",ftp_typ);
@@ -13051,7 +12999,7 @@ Please confirm output file specification or supply an alternative:";
         }
 #ifdef FTP_TIMEOUT
 	if (c == -3) {
-            debug(F100,"ftp recvrequest timeout","",0); 
+            debug(F100,"ftp recvrequest timeout","",0);
             bytes = (CK_OFF_T)-1;
 	    ftp_timed_out = 1;
 	    ftpcode = -3;
@@ -13129,8 +13077,8 @@ Please confirm output file specification or supply an alternative:";
 #endif /* COMMENT */
 
 #ifdef OS2
-                if ( out2screen &&            /* we're translating to UCS-2 */ 
-                     !k95stdout && !inserver) /* for the real screen... */     
+                if ( out2screen &&            /* we're translating to UCS-2 */
+                     !k95stdout && !inserver) /* for the real screen... */
                 {
                     union {
                         USHORT ucs2;
@@ -13148,7 +13096,7 @@ Please confirm output file specification or supply an alternative:";
                                        &colorcmd
                                        );
 
-                } else 
+                } else
 #endif /* OS2 */
                 {
                     if ((x = xpnbyte(c0,TC_UCS2,ftprecv.fcs,fn)) < 0) break;
@@ -13271,7 +13219,7 @@ Please confirm output file specification or supply an alternative:";
     close(ftprecv.din);
 #endif /* TCPIPLIB */
     ftprecv.reply = getreply(0,ftprecv.fcs,ftprecv.rcs,ftp_vbm,0);
-    ftprecvret = ((ftpcode < 0 || ftprecv.reply == REPLY_TRANSIENT || 
+    ftprecvret = ((ftpcode < 0 || ftprecv.reply == REPLY_TRANSIENT ||
                    ftprecv.reply == REPLY_ERROR) ? -1 : 0);
 #ifdef NTSIG
      ckThreadEnd(threadinfo);
@@ -13358,7 +13306,7 @@ recvrequest(cmd,local,remote,lmode,printnames,recover,pipename,xlate,fcs,rcs)
 #ifdef COMMENT
     if (!out2screen && !pipename && ftprecv.is_retr) { /* To real file */
         if (recursive && ckstrchr(local,'/')) {
-	    
+
         }
     }
 #endif /* COMMENT */
@@ -14756,7 +14704,7 @@ setpbsz(size) unsigned int size;
     } else
       maxbuf = actualbuf;
     ucbufsiz = maxbuf - FUDGE_FACTOR;
-    debug(F101,"setpbsz ucbufsiz","",ucbufsiz);    
+    debug(F101,"setpbsz ucbufsiz","",ucbufsiz);
     reply_parse = NULL;
     return(0);
 }
@@ -14981,7 +14929,7 @@ fts_cpl( int x )
 fts_cpl(x) int x;
 #endif /* CK_ANSIC */
 {
-    if (!auth_type 
+    if (!auth_type
 #ifdef OS2
          || !ck_crypt_is_installed()
 #endif /* OS2 */
@@ -15567,7 +15515,7 @@ ftp_login(host) char * host;
               else
                 ckmakmsg(prompt,PROMPTSIZ," Name (",host,"): ",NULL);
               tmp[0] = '\0';
-              
+
               ok = uq_txt(NULL,prompt,1,NULL,tmp,PASSBUFSIZ,NULL,
                           DEFAULT_UQ_TIMEOUT);
               if (!ok || *tmp == '\0')
@@ -16035,14 +15983,14 @@ remote_files(new_query, arg, pattern, proxy_switch)
 	if (!forced || !mgetmethod)	/* Not forced... */
 	  mgetmethod = (sfttab[0] && sfttab[SFT_MLST]) ? /* so pick one */
 	      SND_MLS :
-	      SND_NLS; 
-/*                                           
+	      SND_NLS;
+/*
   User's Command:                 Result:
     mget /nlst                     NLST (NULL)
     mget /nlst foo                 NLST foo
-    mget /nlst *.txt               NLST *.txt 
+    mget /nlst *.txt               NLST *.txt
     mget /nlst /match:*.txt        NLST (NULL)
-    mget /nlst /match:*.txt  foo   NLST foo   
+    mget /nlst /match:*.txt  foo   NLST foo
     mget /mlsd                     MLSD (NULL)
     mget /mlsd foo                 MLSD foo
     mget /mlsd *.txt               MLSD (NULL)
@@ -16161,7 +16109,7 @@ remote_files(new_query, arg, pattern, proxy_switch)
 	    rfrc = -3;			/* means we're done */
 	    return(NULL);
 	}
-	printf("POPPING(%d)...\n",mlsdepth-1); 
+	printf("POPPING(%d)...\n",mlsdepth-1);
 	if (tmpfilnam[mlsdepth]) free(tmpfilnam[mlsdepth]);
 	mlsdepth--;
 	doftpcdup();
@@ -16214,7 +16162,7 @@ remote_files(new_query, arg, pattern, proxy_switch)
 	    if (mlsdepth < MLSDEPTH) {
 		char * p2 = NULL;
 		mlsdepth++;
-		printf("RECURSING [%s](%d)...\n",p,mlsdepth); 
+		printf("RECURSING [%s](%d)...\n",p,mlsdepth);
 		if (doftpcwd(p,0) > 0) {
 		    int x;
 		    if (!ckstrchr(p,'/')) {
@@ -16262,8 +16210,8 @@ remote_files(new_query, arg, pattern, proxy_switch)
     if (deblog) {
 	debug(F101,"remote_files havesize","",havesize);
 	debug(F101,"remote_files havetype","",havetype);
-	debug(F110,"remote_files havemdtm",havemdtm,0);	
-	debug(F110,"remote_files name",p,0);	
+	debug(F110,"remote_files havemdtm",havemdtm,0);
+	debug(F110,"remote_files name",p,0);
     }
 #endif /* DEBUG */
     return((CHAR *)p);
@@ -16443,7 +16391,7 @@ secure_write(fd, buf, nbyte)
 {
     int ret;
 
-#ifdef FTP_TIMEOUT    
+#ifdef FTP_TIMEOUT
     ftp_timed_out = 0;
     if (check_data_connection(fd,1) < 0) {
 	ftp_timed_out = 1;
@@ -16924,7 +16872,7 @@ secure_getc(fd,fc) int fd,fc;
                 nin = bufp = 0;
 		ftp_timed_out = 1;
 		return(-3);
-	    }		
+	    }
 #endif	/* FTP_TIMEOUT */
 
             nin = bufp = recv(fd,(char *)ucbuf,actualbuf,0);
